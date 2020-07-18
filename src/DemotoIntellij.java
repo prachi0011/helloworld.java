762dadd6f9fe719823226b0441a4d36a1ce4525f
prachi0011 <prachi.agrawal_cs19@gla.ac.in>
2020-07-17 22:56:16 -0700

    initial commit

---
+++ src/DemotoIntellij.java
@@ -0,0 +1,6 @@
+public class DemotoIntellij {
+     public static void main(String[] args) {
+         System.out.println("Welcome to Intellij");
+
+    }
+}
---
+++ src/demoscanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Demoscannar {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your full name");
+        String name =scn.nextLine();
+        System.out.println("Enter your age");
+        int _age_ = scn.nextInt();
+        
+        
+    }
+}
